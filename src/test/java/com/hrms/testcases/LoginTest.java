7cea25a9d4310ac7e0fa507c3a11d36afb9e9dcb
SelmaKarapicak <karapicakselma@gmail.com>
2020-05-30 15:43:44 -0400

    HrmsFramework Maven Projects

---
+++ src/test/java/com/hrms/testcases/LoginTest.java
@@ -0,0 +1,57 @@
+package com.hrms.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.hrms.utils.CommonMethods;
+import com.hrms.utils.ConfigsReader;
+
+public class LoginTest extends CommonMethods{
+	
+//	@BeforeMethod
+//	public void openBrowser() {
+//		setUp();
+//		initialize();
+//	}
+//	
+//	@AfterMethod
+//	public void closeBrowser() {
+//		tearDown();
+//	}
+	
+	@Test(groups="smoke")
+	public void validAdminLogin() {
+		//LoginPageElements login=new LoginPageElements();
+		sendText(login.username, ConfigsReader.getProperty("username"));
+		sendText(login.password, ConfigsReader.getProperty("password"));
+		click(login.loginBtn);
+		
+		//DashBoardPageElements db=new DashBoardPageElements();
+		String expectedUser="Welcome Admin";
+		String actualUser=dashboard.welcome.getText();
+		Assert.assertEquals(actualUser, expectedUser, "Admin is NOT Logged in");
+		Assert.assertTrue(actualUser.contains(ConfigsReader.getProperty("username")));	
+	}
+	
+	@Test(groups="regression")
+	public void invalidPasswordLogin() {
+		//LoginPageElements login=new LoginPageElements();
+		sendText(login.username, ConfigsReader.getProperty("username"));
+		sendText(login.password, "uiuguig");
+		click(login.loginBtn);
+		
+		String expected="Invalid credentials";
+		Assert.assertEquals(login.errorMsg.getText(), expected,"Error message text is not matched");	
+	}
+	
+	@Test(groups="regression")
+	public void emptyUsernameLogin() {
+		//LoginPageElements login=new LoginPageElements();
+		sendText(login.password, ConfigsReader.getProperty("password"));
+		click(login.loginBtn);
+		
+		String expected = "User name cannot be empty";
+		Assert.assertEquals(login.errorMsg.getText(), expected,"Error message is not matched ");
+	}
+
+}
---
+++ src/test/java/com/hrms/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.hrms.utils;
+
+public class Constants {
+	
+	public static final String CHROME_DRIVER_PATH=System.getProperty("user.dir")+ "\\src\\test\\resources\\drivers\\chromedriver.exe";
+	public static final String GECKO_DRIVER_PATH=System.getProperty("user.dir") +"\\src\\test\\resources\\drivers\\geckodriver.exe";
+
+	public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+ "\\src\\test\\resources\\configs\\Configuration.properties";
+	
+	public static final int IMPLICIT_WAIT_TIME=10;
+	
+	public static final int EXPLICIT_WAIT_TIME=30;
+	
+	public static final String TESTDATA_FILEPATH=System.getProperty("user.dir")+"/testdata/Book1.xlsx";
+
+
+}
